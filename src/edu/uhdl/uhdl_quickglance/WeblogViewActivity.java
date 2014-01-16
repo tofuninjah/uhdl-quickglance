31bdaab842502147e6e063a3193a06f217eadcdc
Chung Kang <cykang@central.uh.edu>
2014-01-16 17:51:14 -0600

    started adding progress bar.

--- src/edu/uhdl/uhdl_quickglance/WeblogViewActivity.java
+++ src/edu/uhdl/uhdl_quickglance/WeblogViewActivity.java
@@ -1,14 +1,19 @@
 package edu.uhdl.uhdl_quickglance;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ProgressBar;
 
 public class WeblogViewActivity extends Activity {
+
+    protected ProgressBar mProgressBar;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weblog_view);
+        
+        mProgressBar = (ProgressBar) findViewById(R.id.progressBar1);
         
     
     }
