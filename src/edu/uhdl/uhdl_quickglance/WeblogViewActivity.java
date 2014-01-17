d26a739c0e079a29449731049db675dd21670131
Chung Kang <cykang@central.uh.edu>
2014-01-17 17:52:17 -0600

    Check network state.

--- src/edu/uhdl/uhdl_quickglance/WeblogViewActivity.java
+++ src/edu/uhdl/uhdl_quickglance/WeblogViewActivity.java
@@ -1,8 +1,13 @@
 package edu.uhdl.uhdl_quickglance;
 
 import android.app.Activity;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 public class WeblogViewActivity extends Activity {
 
@@ -15,8 +20,28 @@ public class WeblogViewActivity extends Activity {
         
         mProgressBar = (ProgressBar) findViewById(R.id.progressBar1);
         
+        if (isNetworkAvailable()){
+			mProgressBar.setVisibility(View.VISIBLE);
+			//GetBlogPostsTask getBlogPostsTask = new GetBlogPostsTask();
+			//getBlogPostsTask.execute();
+		} else {
+			Toast.makeText(this, "Network is unavailable", Toast.LENGTH_LONG).show();
+		}
     
     }
+    
+    private boolean isNetworkAvailable() {
+		ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+		NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+		
+		boolean isAvailable = false;
+		
+		if(networkInfo != null && networkInfo.isConnected()){
+			isAvailable = true;
+		}
+		
+		return isAvailable;
+	}
 
 
 }
